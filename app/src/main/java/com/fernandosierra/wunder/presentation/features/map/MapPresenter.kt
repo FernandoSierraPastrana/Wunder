a98c96fd443ce0776470c7d9234eb907c6864aac
Fernando Sierra <fernando.sierra.pastrana@gmail.com>
2018-02-08 21:16:21 -0600

    Map draw
    Add of marker icon
    Add of Focused view state

--- app/src/main/java/com/fernandosierra/wunder/presentation/features/map/MapPresenter.kt
+++ app/src/main/java/com/fernandosierra/wunder/presentation/features/map/MapPresenter.kt
@@ -5,32 +5,46 @@ import com.fernandosierra.wunder.presentation.Presenter
 import com.fernandosierra.wunder.presentation.ViewState
 import com.fernandosierra.wunder.presentation.ViewState.Error
 import com.fernandosierra.wunder.presentation.features.map.MapView.MapViewState
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.Marker
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.async
 import javax.inject.Inject
 
-class MapPresenter @Inject constructor(view: MapView, private val locationsInteractor: LocationsInteractor)
-    : Presenter<MapView>(view), OnMapReadyCallback {
-
+class MapPresenter @Inject constructor(view: MapView, private val locationsInteractor: LocationsInteractor) : Presenter<MapView>(view) {
+    private var isMapReady = false
     fun init() {
-        view.state = MapViewState.Init(this)
+        view.state = MapViewState.Init(createOnMapReadyCallback())
+    }
+
+    private fun createOnMapReadyCallback(): OnMapReadyCallback {
+        return OnMapReadyCallback {
+            if (it == null) {
+                view.state = Error(RuntimeException("Unable to fetch Google Map"))
+            } else {
+                isMapReady = true
+                view.state = MapViewState.Ready(it, createOnMarkerFocused(), {})
+                updateLocations()
+            }
+        }
+    }
+
+    private fun createOnMarkerFocused(): (Marker) -> Boolean {
+        return {
+            view.state = MapViewState.Focused(it)
+            true
+        }
     }
 
     fun updateLocations() {
-        manageJob(async(UI) {
-            try {
-                view.state = ViewState.Success(locationsInteractor.getAll())
-            } catch (throwable: Throwable) {
-                view.state = Error(throwable)
-            }
-        })
-    }
-
-    override fun onMapReady(googleMap: GoogleMap?) = if (googleMap != null) {
-        view.state = MapViewState.Ready(googleMap)
-    } else {
-        view.state = Error(RuntimeException("Unable to fetch Google Map"))
+        if (isMapReady) {
+            manageJob(async(UI) {
+                try {
+                    view.state = ViewState.Success(locationsInteractor.getAll())
+                } catch (throwable: Throwable) {
+                    view.state = Error(throwable)
+                }
+            })
+        }
     }
 }
--- app/src/main/java/com/fernandosierra/wunder/presentation/features/map/MapView.kt
+++ app/src/main/java/com/fernandosierra/wunder/presentation/features/map/MapView.kt
@@ -4,13 +4,14 @@ import com.fernandosierra.wunder.R
 import com.fernandosierra.wunder.domain.model.Placemark
 import com.fernandosierra.wunder.presentation.ActivityView
 import com.fernandosierra.wunder.presentation.ViewState
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import timber.log.Timber
 import javax.inject.Inject
 import kotlin.properties.Delegates
 
@@ -18,16 +19,12 @@ class MapView @Inject constructor(activity: MapActivity) : ActivityView<MapActiv
     private lateinit var mapFragment: SupportMapFragment
     private lateinit var googleMap: GoogleMap
     private lateinit var markers: MutableList<Marker>
-    override var state by Delegates.observable<ViewState>(ViewState.Created()) { value, old, new ->
+    override var state by Delegates.observable<ViewState>(ViewState.Created()) { _, _, new ->
         when (new) {
             is MapViewState.Init -> onInit()
             is MapViewState.Ready -> onReady()
-            is ViewState.Success<*> ->
-                if (old is MapViewState.Ready) {
-                    onSuccess()
-                } else {
-                    throw UnsupportedOperationException("Wrong state change")
-                }
+            is ViewState.Success<*> -> onSuccess()
+            is MapViewState.Focused -> onFocused()
             is ViewState.Error -> onError()
         }
     }
@@ -48,24 +45,39 @@ class MapView @Inject constructor(activity: MapActivity) : ActivityView<MapActiv
         markers = successState.map {
             val placemark = it as Placemark
             googleMap.addMarker(MarkerOptions().position(LatLng(placemark.latitude, placemark.longitude))
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_car))
                     .title(placemark.name))
         }.toMutableList()
     }
 
     override fun onError() {
+        Timber.e((state as ViewState.Error).error)
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     private fun onReady() {
-        googleMap = (state as MapViewState.Ready).googleMap
+        val readyState = state as MapViewState.Ready
+        googleMap = readyState.googleMap
         googleMap.uiSettings.isMapToolbarEnabled = false
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        googleMap.setOnMapClickListener(readyState.mapClickCallback)
+        googleMap.setOnMarkerClickListener(readyState.markerClickCallback)
+    }
+
+    private fun onFocused() {
+        markers.map {
+            if (it.title != (state as MapViewState.Focused).focusedMarker.title) {
+                it.isVisible = false
+            }
+        }
     }
 
     sealed class MapViewState : ViewState() {
         class Init(val callback: OnMapReadyCallback) : MapViewState()
-        class Ready(val googleMap: GoogleMap) : MapViewState()
+
+        class Ready(val googleMap: GoogleMap,
+                    val markerClickCallback: (Marker) -> Boolean,
+                    val mapClickCallback: (LatLng) -> Unit) : MapViewState()
+
+        class Focused(val focusedMarker: Marker) : MapViewState()
     }
 }
